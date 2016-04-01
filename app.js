724d5af0aefc96b51e21185f1067e2840d2d6e6f
Tony Tang <tony_tang@epam.com>
2016-04-01 18:45:35 +0800

    Add exec scripts

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ const options = {
     "auth-user": "burberry",
     "auth-pass": "",
     "timeout": 3600,
+    "har": ".har"
 };
 
 forEachAsync(data.links, link => {
     var cnlink = data.host + link + data.query[1];
+    options.har = link + ".har";
 
     phantomas(cnlink, options, function(err, json, results) {
         // err: exit code
@@ -26,5 +28,21 @@ forEachAsync(data.links, link => {
 
 var formatResult = function(results) {
     var timeElapsed = results.metrics.httpTrafficCompleted / 1000;
-    return results.url + ", " + timeElapsed + "s";
+    var domLoaded = results.metrics.domContentLoaded / 1000;
+    var domComplete = results.metrics.domComplete / 1000;
+    return results.url +
+        ", Requests: " + results.metrics.requests +
+        ", Time: " + timeElapsed + "s" +
+        ", domContentLoaded: " + domLoaded + "s" +
+        ", domComplete: " + domComplete + "s";
 }
+
+// To generate csv and har
+// const exec = require('child_process').exec;
+// const cmd = 'phantomas "https://cn.stg.apac.burberry.com/" --auth-user=burberry ' +
+//     '--auth-pass="" -R csv:timestamp > result.csv ' +
+//     '--timeout=3600 --har=result.json';
+//
+// exec(cmd, function(error, stdout, stderr) {
+//     // command output is in stdout
+// });
